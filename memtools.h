84b455442ab22fca946d694753de1630df421c2c
Delta <46466697+DeltaGW2@users.noreply.github.com>
2025-07-13 15:24:42 +0200

    FollowJmpChain function.

--- memtools.h
+++ memtools.h
@@ -37,9 +37,47 @@ namespace memtools
 	///----------------------------------------------------------------------------------------------------
 	inline void* FollowRelativeAddress(void* aAddress)
 	{
-		int32_t jmpOffset = *(int32_t*)aAddress;
+		int32_t jmpOffset = *(__unaligned int32_t*)aAddress;
 
 		return (PBYTE)aAddress + jmpOffset + 4;
+	}
+
+	inline void* FollowJmpChain(PBYTE aPointer)
+	{
+		while (true)
+		{
+			if (aPointer[0] == 0xEB)
+			{
+				/* short jmp */
+				/* address is relative to after jmp */
+				aPointer += 2 + *(__unaligned int8_t*) & aPointer[1]; // jmp +imm8
+			}
+			else if (aPointer[0] == 0xE9)
+			{
+				/* near jmp */
+				/* address is relative to after jmp */
+				aPointer += 5 + *(__unaligned int32_t*) &aPointer[1]; // jmp +imm32
+			}
+			else if (aPointer[0] == 0xFF && aPointer[1] == 0x25)
+			{
+				/* far jmp */
+				/* x64: address is relative to after jmp */
+				/* x86: absolute address can be read directly */
+#ifdef _WIN64
+				aPointer += 6 + *(__unaligned int32_t*) &aPointer[2]; // jmp [+imm32]
+#else
+				aPointer = *(__unaligned int32_t*) &aPointer[2]; // jmp [imm32]
+#endif
+				/* dereference to get the actual target address */
+				aPointer = *(__unaligned PBYTE*)aPointer;
+			}
+			else
+			{
+				break;
+			}
+		}
+
+		return aPointer;
 	}
 
 	///----------------------------------------------------------------------------------------------------
